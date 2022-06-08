bae1c23b9a714d34e73b941bf5196647b452fa45
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2022-06-08 14:09:02 +0200

    Add new reflection methods

--- src/main/java/com/thebuzzmedia/exiftool/commons/reflection/ClassUtils.java
+++ src/main/java/com/thebuzzmedia/exiftool/commons/reflection/ClassUtils.java
@@ -17,6 +17,10 @@
 
 package com.thebuzzmedia.exiftool.commons.reflection;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
 /**
  * Static Class Utilities.
  */
@@ -24,6 +28,110 @@ public final class ClassUtils {
 
 	// Ensure non instantiation.
 	private ClassUtils() {
+	}
+
+	/**
+	 * Get given class if available on classpath, otherwise throw {@link ReflectionException}.
+	 *
+	 * @param klass Class name.
+	 * @return The class instance if available.
+	 * @throws ReflectionException If given class is not found in the classpath.
+	 */
+	public static Class<?> lookupClass(String klass) {
+		try {
+			return Class.forName(klass);
+		}
+		catch (ClassNotFoundException ex) {
+			throw new ReflectionException(ex);
+		}
+	}
+
+	/**
+	 * Find static method with given return type parameter types on given class.
+	 *
+	 * @param klass The class.
+	 * @param methodName The method name.
+	 * @param returnType The method return type.
+	 * @param parameterTypes Parameter types.
+	 * @return The method.
+	 * @throws ReflectionException If given method does not exist.
+	 * @throws ReflectionException If given method is not accessible.
+	 */
+	public static MethodHandle findStaticMethod(Class<?> klass, String methodName, Class<?> returnType, Class<?>... parameterTypes) {
+		MethodHandles.Lookup lookup = MethodHandles.publicLookup();
+		MethodType mt = MethodType.methodType(returnType, parameterTypes);
+
+		try {
+			return lookup.findStatic(klass, methodName, mt);
+		}
+		catch (NoSuchMethodException | IllegalAccessException ex) {
+			throw new ReflectionException(ex);
+		}
+	}
+
+	/**
+	 * Find non static method with given return type parameter types on given class.
+	 *
+	 * @param klass The class.
+	 * @param methodName The method name.
+	 * @param returnType The method return type.
+	 * @param parameterTypes Parameter types.
+	 * @return The method.
+	 * @throws ReflectionException If given method does not exist.
+	 * @throws ReflectionException If given method is not accessible.
+	 */
+	public static MethodHandle findMethod(Class<?> klass, String methodName, Class<?> returnType, Class<?>... parameterTypes) {
+		MethodHandles.Lookup lookup = MethodHandles.publicLookup();
+		MethodType mt = MethodType.methodType(returnType, parameterTypes);
+
+		try {
+			return lookup.findVirtual(klass, methodName, mt);
+		}
+		catch (NoSuchMethodException | IllegalAccessException ex) {
+			throw new ReflectionException(ex);
+		}
+	}
+
+	/**
+	 * Invoke given static method.
+	 *
+	 * @param method The method to invoke.
+	 * @param args Method parameters.
+	 * @return The method result.
+	 * @throws ReflectionException If an error occurs while calling method.
+	 */
+	public static Object invokeStatic(MethodHandle method, Object... args) {
+		return doInvoke(method, args);
+	}
+
+	/**
+	 * Invoke given method.
+	 *
+	 * @param method The method to invoke.
+	 * @param target The target instance.
+	 * @param args Method parameters.
+	 * @return The method result.
+	 * @throws ReflectionException If an error occurs while calling method.
+	 */
+	public static Object invoke(MethodHandle method, Object target, Object... args) {
+		return doInvoke(method.bindTo(target), args);
+	}
+
+	/**
+	 * Invoke given method with given arguments.
+	 *
+	 * @param method The method to invoke.
+	 * @param args Method parameters.
+	 * @return The method result.
+	 * @throws ReflectionException If an error occurs while calling method.
+	 */
+	private static Object doInvoke(MethodHandle method, Object... args) {
+		try {
+			return method.invokeWithArguments(args);
+		}
+		catch (Throwable ex) {
+			throw new ReflectionException(ex);
+		}
 	}
 
 	/**
@@ -39,6 +147,12 @@ public final class ClassUtils {
 		}
 		catch (ClassNotFoundException ex) {
 			return false;
+		}
+	}
+
+	public static class ReflectionException extends RuntimeException {
+		private ReflectionException(Throwable cause) {
+			super(cause);
 		}
 	}
 }
--- src/test/java/com/thebuzzmedia/exiftool/commons/reflection/ClassUtilsTest.java
+++ src/test/java/com/thebuzzmedia/exiftool/commons/reflection/ClassUtilsTest.java
@@ -17,11 +17,79 @@
 
 package com.thebuzzmedia.exiftool.commons.reflection;
 
+import com.thebuzzmedia.exiftool.commons.reflection.ClassUtils.ReflectionException;
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.Test;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ClassUtilsTest {
+
+	@Test
+	public void it_should_get_class() {
+		ThrowableAssert.ThrowingCallable lookupNotFoundClass = new ThrowableAssert.ThrowingCallable() {
+			@Override
+			public void call() {
+				ClassUtils.lookupClass("com.thebuzzmedia.exiftool.commons.FooBar");
+			}
+		};
+
+		assertThat(ClassUtils.lookupClass("com.thebuzzmedia.exiftool.commons.reflection.ClassUtils")).isEqualTo(ClassUtils.class);
+		assertThatThrownBy(lookupNotFoundClass).isInstanceOf(ReflectionException.class).hasCauseInstanceOf(ClassNotFoundException.class);
+	}
+
+	@Test
+	public void it_should_get_static_method() {
+		assertThat(ClassUtils.findStaticMethod(TestClass.class, "noop", void.class)).isNotNull();
+		assertThat(ClassUtils.findStaticMethod(TestClass.class, "isEmpty", boolean.class, String.class)).isNotNull();
+
+		ThrowableAssert.ThrowingCallable findUnknownMethod = new ThrowableAssert.ThrowingCallable() {
+			@Override
+			public void call() throws Throwable {
+				ClassUtils.findStaticMethod(TestClass.class, "unknown_static_method", void.class);
+			}
+		};
+
+		assertThatThrownBy(findUnknownMethod).isInstanceOf(ReflectionException.class).hasCauseInstanceOf(NoSuchMethodException.class);
+	}
+
+	@Test
+	public void it_should_get_method() {
+		assertThat(ClassUtils.findMethod(TestClass.class, "getId", int.class)).isNotNull();
+
+		ThrowableAssert.ThrowingCallable findUnknownMethod = new ThrowableAssert.ThrowingCallable() {
+			@Override
+			public void call() throws Throwable {
+				ClassUtils.findMethod(TestClass.class, "unknown_static_method", void.class);
+			}
+		};
+
+		assertThatThrownBy(findUnknownMethod).isInstanceOf(ReflectionException.class).hasCauseInstanceOf(NoSuchMethodException.class);
+	}
+
+	@Test
+	public void it_should_invoke_static_method() throws Exception {
+		MethodHandle methodHandle = MethodHandles.publicLookup().findStatic(TestClass.class, "isEmpty", MethodType.methodType(boolean.class, String.class));
+		assertThat(ClassUtils.invokeStatic(methodHandle, "")).isEqualTo(true);
+		assertThat(ClassUtils.invokeStatic(methodHandle, "not_empty_string")).isEqualTo(false);
+	}
+
+	@Test
+	public void it_should_invoke_instance_method() throws Exception {
+		TestClass target = new TestClass();
+		MethodHandle methodHandle_getId = MethodHandles.publicLookup().findVirtual(TestClass.class, "getId", MethodType.methodType(int.class));
+		assertThat(ClassUtils.invoke(methodHandle_getId, target)).isEqualTo(0);
+
+		MethodHandle methodHandle_setId = MethodHandles.publicLookup().findVirtual(TestClass.class, "setId", MethodType.methodType(void.class, int.class));
+		int newId = 10;
+		ClassUtils.invoke(methodHandle_setId, target, newId);
+		assertThat(target.getId()).isEqualTo(newId);
+	}
 
 	@Test
 	public void it_should_check_if_class_is_available() {
